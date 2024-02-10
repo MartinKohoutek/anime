a36ceb97f7c57a5ff139535813c4a4f0174ed875
Martin Kohoutek <martinkohoutek@hotmail.com>
2024-02-10 16:47:26 +0100

    [Admin] Sidebar - Removed Unused Items

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -9,16 +9,6 @@
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="{{ !empty(Auth::user()->photo) ? asset(Auth::user()->photo) : asset('upload/no_image.jpg') }}"
-                    class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="{{ route('admin.profile') }}" class="d-block">{{ Auth::user()->name }}</a>
-            </div>
-        </div>
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
