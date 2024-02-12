bf2a88c0f80cb0892da10fc06d9cb7e04c3e34d2
Martin Kohoutek <martinkohoutek@hotmail.com>
2024-02-12 23:37:12 +0100

    [Frontend] Forgot Password Page Added

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,68 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@extends('frontend.layout.master')
+@section('title', 'Login')
+@section('content')
+    <!-- Normal Breadcrumb Begin -->
+    <section class="normal-breadcrumb set-bg" data-setbg="{{ asset('frontend/img/normal-breadcrumb.jpg') }}">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="normal__breadcrumb__text">
+                        <h2>Forgot password</h2>
+                        <p>Forgot your password? No problem. Just let us know your email address and we will email you a
+                            password reset link that will allow you to choose a new one.'</p>
+                    </div>
+                </div>
+            </div>
         </div>
+    </section>
+    <!-- Normal Breadcrumb End -->
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+    <!-- Login Section Begin -->
+    <section class="login spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="login__form">
+                        <h3>Forgot password</h3>
+                        <form action="#" method="POST" action="{{ route('password.email') }}">
+                            @csrf
+                            <div class="input__item">
+                                <input type="email" name="email" id="email" placeholder="Email address"
+                                    value="{{ old('email') }}">
+                                <span class="icon_mail"></span>
+                            </div>
+
+                            <button type="submit" class="site-btn">EmailPassword Reset Link</button>
+                        </form>
+                        <a href="{{ route('login') }}" class="forget_pass" style="bottom: -50px">Back to Login</a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="login__register">
+                        <h3>Dont’t Have An Account?</h3>
+                        <a href="{{ route('register') }}" class="primary-btn">Register Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="login__social">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-lg-6">
+                        <div class="login__social__links">
+                            <span>or</span>
+                            <ul>
+                                <li><a href="#" class="facebook"><i class="fa fa-facebook"></i> Sign in With
+                                        Facebook</a></li>
+                                <li><a href="#" class="google"><i class="fa fa-google"></i> Sign in With
+                                        Google</a></li>
+                                <li><a href="#" class="twitter"><i class="fa fa-twitter"></i> Sign in With
+                                        Twitter</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </form>
-</x-guest-layout>
+    </section>
+    <!-- Login Section End -->
+@endsection
